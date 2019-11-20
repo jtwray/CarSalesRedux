4e9f0c7e4fc1f46089c9dce904bad2c7639ab6f4
tucker <tuckerwray84@gmail.com>
2019-11-20 00:10:06 -0500

    add react-redux redux to dependency list / import reducer initialState createStore and provider to outer/upmost component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,32 @@
-initialState={
+const initialState = {
 
-}
+    additionalPrice: 0,
+    car: {
+        price: 26395,
+        name: '2019 Ford Mustang',
+        image:
+            'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+        features: []
+    },
+    additionalFeatures: [
+        { id: 1, name: 'V-6 engine', price: 1500 },
+        { id: 2, name: 'Racing detail package', price: 1500 },
+        { id: 3, name: 'Premium sound system', price: 500 },
+        { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
 
-const reducer(state=initialState,action){
-    switch(action.type){
-        case ADD_FEAT:
-            const newFeature=action.payload;
-            return{
+};
+
+const reducer=(state = initialState, action)=>{
+    switch (action.type) {
+        case "ADD_FEAT":
+            const newFeature = action.payload;
+            return {
                 ...state,
-                featuresList:[...state.featuresList, newFeature]
+                featuresList: [...state.featuresList, newFeature]
             }
-            default:
-                return state;
+        default:
+            return state;
     }
 }
 export default reducer;
